b11ca63e09925ad44f10f59fb2577d4e8003084b
tresende <tresende@br.ibm.com>
2018-05-31 17:22:56 -0300

    Salvando foto da camera

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -1,18 +1,22 @@
 package br.com.alura.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.*;
+
+import java.io.File;
 
 import br.com.alura.agenda.DAO.AlunoDAO;
 import br.com.alura.agenda.modelo.Aluno;
 
 public class FormularioActivity extends AppCompatActivity {
-
 
     private FormularioHelper helper;
 
@@ -21,12 +25,23 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
         helper = new FormularioHelper(this);
         if (aluno != null) {
             helper.preencheFormulario(aluno);
         }
+        Button butaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
+        butaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/ + " + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivity(intentCamera);
+            }
+        });
     }
 
     @Override
