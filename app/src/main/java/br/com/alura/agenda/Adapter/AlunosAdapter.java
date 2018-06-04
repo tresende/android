85022800094896668bc9d7db41e896f1916012ef
tresende <tresende@br.ibm.com>
2018-06-04 07:24:45 -0300

    Layout

--- app/src/main/java/br/com/alura/agenda/Adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/alura/agenda/Adapter/AlunosAdapter.java
@@ -1,13 +1,18 @@
 package br.com.alura.agenda.Adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import br.com.alura.agenda.R;
 import br.com.alura.agenda.modelo.Aluno;
 
 public class AlunosAdapter extends BaseAdapter {
@@ -37,9 +42,29 @@ public class AlunosAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(this.context);
         Aluno aluno = this.alunos.get(position);
-        view.setText(aluno.toString());
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        View view = convertView;
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
         return view;
     }
 }
