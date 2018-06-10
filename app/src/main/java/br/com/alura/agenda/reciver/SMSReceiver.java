8d1d53de2e8b5f34e170cc8a08a06a215acd2667
Thiago Resende <thiago@Thiagos-MacBook-Air.local>
2018-06-10 10:32:06 -0300

    Tratando eventos de sistema

--- app/src/main/java/br/com/alura/agenda/reciver/SMSReceiver.java
+++ app/src/main/java/br/com/alura/agenda/reciver/SMSReceiver.java
@@ -1,8 +1,17 @@
 package br.com.alura.agenda.reciver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 /**
  * Created by thiago on 10/06/18.
  */
 
-public class SMSReceiver {
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
+    }
 }
