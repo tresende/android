57afb6e0876ea1f03cdeb1949f938e3883ec7713
Thiago Resende <thiago.gcresende@gmail.com>
2018-06-23 20:07:06 -0300

    Layout e consumindo um rest

---
+++ app/src/main/java/br/com/alura/agenda/WebClient.java
@@ -0,0 +1,8 @@
+package br.com.alura.agenda;
+
+/**
+ * Created by thiago on 17/06/18.
+ */
+
+public class WebClient {
+}
---
+++ app/src/main/java/br/com/alura/agenda/converter/AlunoConverter.java
@@ -0,0 +1,8 @@
+package br.com.alura.agenda.converter;
+
+/**
+ * Created by thiago on 17/06/18.
+ */
+
+class AlunoConverter {
+}
